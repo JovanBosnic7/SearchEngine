fb9203c74628a72d10aa9510f1553325aaf55705
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2020-02-23 10:27:54 +0100

    #graph Implementiran je usmereni graph

---
+++ datastructures/graph.py
@@ -0,0 +1,30 @@
+class Graph(object):
+
+    def __init__(self):
+        self.odlazeci = {}
+        self.dolazeci = {}
+
+    def insert_vertex(self, e):
+        self.odlazeci[e] = []
+        self.dolazeci[e]= []
+
+    def insert_edge(self, u, v):
+        self.odlazeci[u].append(v)
+        self.dolazeci[v].append(u)
+
+    def __str__(self):
+        string = ""
+        for key in self.odlazeci.keys():
+            list1 = self.odlazeci[key]
+            list2 = self.dolazeci[key]
+
+            string += ("\n*" + str(key) + "out link = " + str(len(list1)) + "in link = " + str(len(list2)))
+            string += "\n"
+
+            for d in list1:
+                string += str("{} -----> {}\n".format(" " * 20, d))
+
+            for d in list2:
+                string += str("{} <---- {}\n".format(" " * 20, d))
+
+        return str(string)
